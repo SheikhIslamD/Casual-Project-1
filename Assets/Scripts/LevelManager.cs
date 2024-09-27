f77bd8b9fa8e5a7040bda4cd19387567e0f2a513
aidane1023 <earnestaidan@gmail.com>
2024-09-27 11:22:16 -0400

    UI Optimization
    Optimize End Screen UI

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,7 +13,7 @@ public class LevelManager : MonoBehaviour
     public int star2Diff, star3Diff;
     Image medal;
     public Sprite gold, silver, bronze, black;
-    TextMeshProUGUI winText, loseText, tiedText, scoreText;
+    TextMeshProUGUI resultText, scoreText;
     Canvas inGameCanvas, endCanvas;
 
     // Arrays to track all active pieces in scene
@@ -29,43 +29,38 @@ public class LevelManager : MonoBehaviour
     }
     public void ScoreRound()
     {
-        //Compare the player score to enemy score
-        //For positive feedback loop maybe use a "star" system. i.e. 1 star for win by 1, 2 starts for win by 2, and 3 for 3+
+        //For positive feedback loop maybe use a "medal" system. i.e. 1 star for win by 1, 2 starts for win by 2, and 3 for 3+
         inGameCanvas.enabled = false;
         endCanvas.enabled = true;
 
         // Show final score
         scoreText.text = "Score: " + (ScoreTracker.playerScore - ScoreTracker.enemyScore);
 
-        // Prep text fields
-        winText.enabled = false;
-        tiedText.enabled = false;
-        loseText.enabled = false;
-
         // If player score meets score requirements, allocate stars
         if (ScoreTracker.playerScore >= ScoreTracker.enemyScore)
         {
             if (ScoreTracker.playerScore - ScoreTracker.enemyScore >= star2Diff)
             {
-                winText.enabled = true;
                 if (ScoreTracker.playerScore - ScoreTracker.enemyScore >= star3Diff)
                 {
-                    // UI 3 star
+                    // UI gold medal
+                    resultText.text = "Amazing!!!";
                     medal.sprite = gold;
                     return;
                 }
-                // UI 2 star
+                // UI silver medal
+                resultText.text = "Pretty Good!";
                 medal.sprite = silver;
                 return;
-            }            
-            // UI 1 star
-            tiedText.enabled = true;
+            }
+            // UI bronze medal
+            resultText.text = "Not Bad";
             medal.sprite = bronze;
             return;
         }
 
         // UI lose Text
-        loseText.enabled = true;
+        resultText.text = "Oh No!";
         medal.sprite = black;
         return;
     }
@@ -98,9 +93,7 @@ public class LevelManager : MonoBehaviour
         endCanvas.enabled = false;
 
         medal = GameObject.Find("Medal").GetComponent<Image>();
-        winText = GameObject.Find("WinText").GetComponent<TextMeshProUGUI>();
-        loseText = GameObject.Find("LoseText").GetComponent<TextMeshProUGUI>();
-        tiedText = GameObject.Find("TiedText").GetComponent<TextMeshProUGUI>();
+        resultText = GameObject.Find("ResultText").GetComponent<TextMeshProUGUI>();
         scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
     }
 }
